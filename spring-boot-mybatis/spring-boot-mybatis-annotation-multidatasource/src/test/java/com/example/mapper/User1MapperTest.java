c74aa88f997d9bb54c715e634c3050a3817968c1
JailBreak02 <542617413@qq.com>
2019-12-30 15:34:31 +0800

    spring-boot-mybatis-annotation-multidatasource project commit --- 运行通过的代码提交 ---

---
+++ spring-boot-mybatis/spring-boot-mybatis-annotation-multidatasource/src/test/java/com/example/mapper/User1MapperTest.java
@@ -0,0 +1,53 @@
+package com.example.mapper;
+
+import com.example.enums.UserSexEnum;
+import com.example.mapper.mysql1.User1Mapper;
+import com.example.model.User;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SpringBootTest
+public class User1MapperTest {
+
+    @Autowired
+    private User1Mapper user1Mapper;
+
+    @Test
+    public void A_TestInsert() {
+        this.user1Mapper.insert(new User("aa", "a123456", UserSexEnum.MAN));
+        this.user1Mapper.insert(new User("bb", "b123456", UserSexEnum.WOMAN));
+        this.user1Mapper.insert(new User("cc", "c123456", UserSexEnum.WOMAN));
+
+        Assert.assertEquals(3, this.user1Mapper.getAll().size());
+    }
+
+    @Test
+    public void B_testQuery() {
+        List<User> users = this.user1Mapper.getAll();
+        if (users == null || users.size() == 0) {
+            System.out.println("is null");
+        } else {
+            System.out.println(users.size());
+        }
+    }
+
+    @Test
+    public void C_testUpdate() {
+        User user = this.user1Mapper.getOne((long) 1);
+        System.out.println(user.toString());
+        user.setNickName("New Por");
+        this.user1Mapper.update(user);
+        System.out.println(user.toString());
+    }
+
+}
